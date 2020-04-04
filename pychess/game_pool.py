e007ed6a8a513109307c090d61654be7b3155e95
juliancoffee <lightdarkdaughter@gmail.com>
2020-04-04 13:07:36 +0300

    Make game_pool more noli  - current_games and history now private

--- pychess/game_pool.py
+++ pychess/game_pool.py
@@ -8,22 +8,21 @@ from history import History
 
 
 class GamePool:
-    current_games: Dict[GameId, Game]
 
     def __init__(self, history: History):
-        self.history = history
-        self.current_games: Dict[GameId, Game] = dict()
+        self._history = history
+        self._current_games: Dict[GameId, Game] = dict()
 
     def start_game(self, tickets: Tuple[Ticket, Ticket]):
-        last_id = self.history.register_game()
+        last_id = self._history.register_game()
         new_game = Game(last_id, tickets)
-        self.current_games[last_id] = new_game
+        self._current_games[last_id] = new_game
 
         return id
 
     def end_game(self, game_id: GameId):
-        ended_game = self.current_games.pop(game_id)
-        self.history.save_replay(ended_game.replay)
+        ended_game = self._current_games.pop(game_id)
+        self._history.save_replay(ended_game.replay)
 
     def get_game(self, game_id: GameId):
-        return self.current_games[game_id]
+        return self._current_games[game_id]
