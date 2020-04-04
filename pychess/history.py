23a8374c59bdcf946f43fa2d0f86442fef3e5c0e
juliancoffee <lightdarkdaughter@gmail.com>
2020-04-04 12:39:59 +0300

    Bug fix in game_pool:     - last_id:         because last_id was unique to all storage, that's incorrect to handle         it in game pool

--- pychess/history.py
+++ pychess/history.py
@@ -2,6 +2,7 @@
 History module
 """
 
+from typing import Optional
 from storage import Storage
 from replay import Replay, ReplayId
 
@@ -10,8 +11,13 @@ class History:
     def __init__(self, storage: Storage):
         self._storage = storage
 
-    def save_replay(self, replay: Replay):
-        self._storage.save_replay(replay)
+    def register_game(self) -> ReplayId:
+        last_gameid = self._storage.count_games()
+        self._storage.save_replay(last_gameid, None)
+        return last_gameid
 
-    def get_replay(self, replay_id: ReplayId) -> Replay:
+    def save_replay(self, replay: Replay):
+        self._storage.save_replay(replay.replay_id, replay)
+
+    def get_replay(self, replay_id: ReplayId) -> Optional[Replay]:
         return self._storage.get_replay(replay_id)
